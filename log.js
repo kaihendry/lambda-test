caa2811a8f2a454b7cc930b73ac16cedf0726f6a
Kai Hendry <hendry@iki.fi>
2021-11-25 14:50:09 +0800

    timestamp in localtime

--- log.js
+++ log.js
@@ -1,12 +1,23 @@
-const log = require("lambda-log");
+const log = require('lambda-log');
+const TZ = require('xtz');
 
 const addLogger = (handler) => {
-  const wrappedHandler = async (...args) => {
-    const mylog = log;
-    mylog.options.meta.ip = args[0].requestContext.identity.sourceIp;
-    return await handler(mylog)(...args);
-  };
-  return wrappedHandler;
+    const wrappedHandler = async (...args) => {
+        const mylog = log;
+        mylog.options.meta.ip = args[0].requestContext.identity.sourceIp;
+
+        mylog.options.dynamicMeta = function (message) {
+            return {
+                timestamp: TZ.toTimeZone(
+                    new Date(),
+                    'Asia/Singapore',
+                ).toISOString(),
+            };
+        };
+
+        return await handler(mylog)(...args);
+    };
+    return wrappedHandler;
 };
 
 module.exports = addLogger;
